6f697ed2c1e9eeeeba8740702fdf7e431fa3cbac
Ali NIZAM <ali.nizam@fsm.edu.tr>
2023-03-09 16:33:37 +0300

    Week2Lab

---
+++ SEN22104E_2022_2023_Spring/src/main/java/Theory02Lab/Greeting.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Theory02Lab;
+
+/**
+ *
+ * @author ali.nizam
+ */
+public class Greeting {
+    String language,message;
+
+    public Greeting(String language, String message) {
+        this.language = language;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return language; // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
