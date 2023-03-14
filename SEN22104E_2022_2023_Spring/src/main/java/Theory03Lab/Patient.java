ca19604c6f5e97e7c0f1dbc507111183dc9f56c7
ali.nizam <ali.nizam@alinizam.fatih.local>
2023-03-14 15:31:01 +0300

    Week 3 Theory

---
+++ SEN22104E_2022_2023_Spring/src/main/java/Theory03Lab/Patient.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Theory03Lab;
+
+/**
+ *
+ * @author ali.nizam
+ */
+public class Patient {
+    int id;
+    String  firstName,lastName,doctorName;
+
+    public Patient(int id, String firstName, String lastName, String doctorName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.doctorName = doctorName;
+    }
+    
+}
